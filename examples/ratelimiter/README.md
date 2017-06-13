d554f16ff5bc5a73c60f21aa056decc3a71c72f0
Peter Marton <peter@risingstack.com>
2017-06-13 16:14:53 +0200

    feat(ratelimiter): add module

---
+++ examples/ratelimiter/README.md
@@ -0,0 +1,31 @@
+## Goal
+
+Rate limiting API.
+
+## Steps
+
+1. Run Redis: see below
+2. `npm install` and `npm start`
+3. Call http://localhost:3001 once and check out response headers
+4. Call http://localhost:3001 more than 5 times and check out response headers
+
+## Requirements
+
+- Docker / Redis
+
+## Redis
+
+### Run
+
+```sh
+docker run -p 6379:6379 redis
+```
+
+## Result
+
+```
+RateLimit-Limit: 5
+RateLimit-Remaining: -1
+RateLimit-Reset: 1497362769
+Retry-After: 51
+```
---
+++ examples/ratelimiter/server.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const express = require('express')
+const Limiter = require('ratelimiter')
+const redis = require('redis')
+const redisClient = redis.createClient()
+
+const app = express()
+const port = process.env.PORT || 3001
+const ratelimiterMax = Number(process.env.RATE_LIMITER_MAX) || 5                    // 5 requests
+const ratelimiterDuration = Number(process.env.RATE_LIMITER_DURATION) || 60 * 1000  // per minute
+
+app.use((req, res, next) => {
+  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  const limiter = new Limiter({
+    db: redisClient,
+    id: clientIP,
+    max: ratelimiterMax,
+    duration: ratelimiterDuration
+  })
+
+  limiter.get((err, limit) => {
+   if (err) {
+     return next(err)
+   }
+
+   res.set('RateLimit-Limit', limit.total)
+   res.set('RateLimit-Remaining', limit.remaining - 1)
+   res.set('RateLimit-Reset', limit.reset)
+
+   if (limit.remaining) {
+     return next()
+   }
+
+   const after = Math.floor(limit.reset - (Date.now() / 1000))
+   res.set('Retry-After', after)
+   res.sendStatus(429)
+
+   return next()
+  })
+})
+
+app.get('/', (req, res, next) => {
+  res.send('ok')
+})
+
+app.listen(port, () => {
+  console.info(`Server listening on port ${port}!`)
+})
